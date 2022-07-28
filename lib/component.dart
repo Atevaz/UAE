04159d0ffcac2c72d757cadc9bb1d0edf390bee9
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-07-28 19:18:38 +0200

    feat: add chat message

--- lib/component.dart
+++ lib/component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:h_m/presentation/widget/chat_message.dart';
 import 'package:h_m/presentation/widget/icon.dart';
 import 'constants/assets_manager.dart';
 import 'presentation/widget/switch_tab_bar.dart';
@@ -12,7 +13,11 @@ class Component extends StatelessWidget {
       child: Scaffold(
           backgroundColor: Colors.blue,
           body: Center(
-            child: StackOver(),
+            child: ChatMessage.reciever(
+                content: 'faskdljkashdkhjfkajlshdfjkhsadkljfhaksjldhfjkadfs',
+                time: '1:26 am',
+                imageAddress:
+                    'https://static.generated.photos/vue-static/face-generator/landing/wall/20.jpg'),
           )),
     );
   }
--- lib/presentation/widget/switch_tab_bar.dart
+++ lib/presentation/widget/switch_tab_bar.dart
@@ -65,7 +65,7 @@ class _StackOverState extends State<StackOver>
                 ],
               ),
             ),
-            Expanded(
+            Expanded( 
               child: TabBarView(
                 controller: _tabController,
                 children: [
