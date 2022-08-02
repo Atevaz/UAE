773ae914570cd1cdec1ed050ef71c4a5ff5cb930
Hazem Osama <hoezuma@gmail.com>
2022-08-02 19:18:28 +0200

    notification item

--- lib/component.dart
+++ lib/component.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:h_m/constants/assets_manager.dart';
-import 'package:h_m/presentation/styles/colors.dart';
-import 'package:h_m/presentation/widget/regular_text.dart';
+import 'presentation/view/notification_item.dart';
 
 
 class HomeWidget extends StatelessWidget {
@@ -11,9 +8,12 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Center(
+        child: NotificationItem(),
+      ),
     );
   }
 }
+
 
 
