2594e100273f1747033bb72fb51932ae22f8d398
Hazem Osama <hoezuma@gmail.com>
2022-08-02 19:39:54 +0200

    notification screen

--- lib/presentation/view/buttom_nav_bar_view.dart
+++ lib/presentation/view/buttom_nav_bar_view.dart
@@ -13,29 +13,27 @@ class _BottomNavBarViewState extends State<BottomNavBarView> {
   @override
   int currentIndex = 0;
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index){
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        showUnselectedLabels: true,
-        unselectedItemColor: AppColor.grey,
-        selectedItemColor: AppColor.teal,
-        backgroundColor: AppColor.white,
-        elevation: 0,
-        selectedFontSize: 16.sp,
-        unselectedFontSize: 16.sp,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_rounded),label:'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite),label:'Fav'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined),label:'Cart'),
-          BottomNavigationBarItem(icon: Icon(Icons.local_offer_rounded),label:'Offers'),
-          BottomNavigationBarItem(icon: Icon(Icons.more_horiz),label:'More'),
-        ],
-      ),
+    return  BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      showUnselectedLabels: true,
+      unselectedItemColor: AppColor.grey,
+      selectedItemColor: AppColor.teal,
+      backgroundColor: AppColor.white,
+      elevation: 0,
+      selectedFontSize: 16.sp,
+      unselectedFontSize: 16.sp,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home_rounded),label:'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite),label:'Fav'),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined),label:'Cart'),
+        BottomNavigationBarItem(icon: Icon(Icons.local_offer_rounded),label:'Offers'),
+        BottomNavigationBarItem(icon: Icon(Icons.more_horiz),label:'More'),
+      ],
     );
   }
 }
