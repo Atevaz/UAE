452239eaf724595d0a776b2325a3d5bb0af877d0
MohanadRaghebDev <mohanad.ragheb.95@gmail.com>
2022-08-03 02:16:32 +0200

    cart screen

--- lib/component.dart
+++ lib/component.dart
@@ -1,33 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:h_m/presentation/widget/default_button.dart';
-import 'package:h_m/presentation/widget/default_form_field.dart';
-import 'package:h_m/presentation/widget/default_phone_number_form_field.dart';
-import 'package:h_m/presentation/widget/otp.dart';
 
 class Component extends StatelessWidget {
   Component({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    GlobalKey<FormState>? formKey = GlobalKey<FormState>();
-    TextEditingController controller1 = TextEditingController();
-    TextEditingController controller2 = TextEditingController();
-    TextEditingController controller3 = TextEditingController();
-    TextEditingController controller4 = TextEditingController();
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Center(
-              child: otp(
-            controller1: controller1,
-            controller2: controller2,
-            controller3: controller3,
-            controller4: controller4,
-            context: context,
-          )),
-        ),
-      ),
-    );
+    return Container();
   }
 }
--- lib/presentation/widget/default_button.dart
+++ lib/presentation/widget/default_button.dart
@@ -2,25 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:h_m/presentation/styles/colors.dart';
 
-Widget defaultButton(
-    {required String label,
-      required Function onPressed,
-      required double fontSize,
-      required context,
-      required bool isExpanded,
-      double? width,
-      double? height,
-      Color? color,
-
-    }) =>
+Widget defaultButton({
+  required String label,
+  required Function onPressed,
+  required double fontSize,
+  required context,
+  required bool isExpanded,
+  double? width,
+  double? height,
+  Color? color,
+  double borderRadius = 25,
+  FontWeight fontWeight = FontWeight.bold,
+  String? fontFamily,
+}) =>
     Container(
       padding: const EdgeInsets.symmetric(horizontal: 5),
-      width: isExpanded ? 350.w: width ?? 202.w,
+      width: isExpanded ? 350.w : width ?? 202.w,
       height: height ?? 65.h,
       clipBehavior: Clip.antiAlias,
       decoration: BoxDecoration(
-        color: color?? AppColor.teal,
-        borderRadius: BorderRadius.circular(25),
+        color: color ?? AppColor.teal,
+        borderRadius: BorderRadius.circular(borderRadius),
       ),
       child: MaterialButton(
         minWidth: 0,
@@ -30,11 +32,11 @@ Widget defaultButton(
         },
         child: Text(
           label,
-          style:  TextStyle(
-            fontSize: fontSize,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+          style: TextStyle(
+              fontSize: fontSize,
+              color: Colors.white,
+              fontWeight: fontWeight,
+              fontFamily: fontFamily),
           textAlign: TextAlign.center,
         ),
       ),
