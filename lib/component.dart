8c4af16cd55fb7b865fc233cb8a2a3e4675190d6
dev0mahmoud <mahmoud000khalil@gmail.com>
2022-08-04 18:40:35 +0200

    offersScreenBuilder

--- lib/component.dart
+++ lib/component.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:h_m/presentation/screens/user/home_layout.dart';
+import 'package:h_m/presentation/screens/user/home_screen.dart';
+import 'package:h_m/presentation/screens/user/offers_screen.dart';
 class Component extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
-    return HomeScreen();
-
+    return OffersScreen();
   }
 }
--- lib/presentation/screens/user/offers_screen.dart
+++ lib/presentation/screens/user/offers_screen.dart
@@ -4,6 +4,10 @@ import 'package:h_m/presentation/styles/colors.dart';
 import 'package:h_m/presentation/widget/custom_app_bar.dart';
 import 'package:h_m/presentation/widget/headline_text.dart';
 
+import '../../view/carousel_view.dart';
+import '../../view/offer_product_banner_builder.dart';
+import '../../widget/offer_product_banner.dart';
+import '../../view/popular_banner.dart';
 import '../../widget/app_bar_notifications_icon.dart';
 import '../../widget/default_form_field.dart';
 import '../../widget/filtter_button.dart';
@@ -22,24 +26,39 @@ class OffersScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(20.0.r),
-        child: Container(
-          height: 43.h,
-          child: Row(
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              Expanded(
-                  child: DefaultFormField(
-                    controller: TextEditingController(),
-                    keyboard: TextInputType.text,
-                    prefixIcon: Icon(Icons.search,color: AppColor.black,),
-                    suffixIcon: Icon(Icons.photo_camera_outlined,color: AppColor.black,),
-                    borderColor: AppColor.white,
-                    focusedBorderColor: AppColor.white,
-                    hintText: 'Search',
-                    fillColor: AppColor.offWhite,
-                  )),
+              Container(
+                height: 43.h,
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: DefaultFormField(
+                          controller: TextEditingController(),
+                          keyboard: TextInputType.text,
+                          prefixIcon: Icon(Icons.search,color: AppColor.black,),
+                          suffixIcon: Icon(Icons.photo_camera_outlined,color: AppColor.black,),
+                          borderColor: AppColor.white,
+                          focusedBorderColor: AppColor.white,
+                          hintText: 'Search',
+                          fillColor: AppColor.offWhite,
+                        )),
+                    Padding(
+                      padding: EdgeInsetsDirectional.only(start: 10.r),
+                      child: FilterButton(),
+                    ),
+                  ],
+                ),
+              ),
               Padding(
-                padding: EdgeInsetsDirectional.only(start: 10.r),
-                child: FilterButton(),
+                padding: EdgeInsets.symmetric(vertical: 20.r),
+                child: CarouselView(),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.r),
+                child: OfferProductBannerBuilder(),
               ),
             ],
           ),
